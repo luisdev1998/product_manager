9e05cf68647a7af935b0feaaaecf976a6d1e197a
Luis carlo Sánchez Tapia <luiscarlosancheztapia@Luiss-MacBook-Air.local>
2023-02-08 18:21:46 -0500

    cambio

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.use('/api/product',cors(), productRoutes);
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
-    console.log("servidor funcionando en el puerto 4000")
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`servidor funcionando en http://127.0.0.1:${PORT}/`);
 });
